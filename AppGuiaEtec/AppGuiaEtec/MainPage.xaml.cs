662e573ee4ab2908e34b8298daae255d3e20524a
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2022-09-28 15:36:25 -0300

    .

--- AppGuiaEtec/AppGuiaEtec/MainPage.xaml.cs
+++ AppGuiaEtec/AppGuiaEtec/MainPage.xaml.cs
@@ -6,43 +6,103 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+using AppGuiaEtec.Views;
+
+
 namespace AppGuiaEtec
 {
-    public partial class MainPage : ContentPage
+    [DesignTimeVisible(false)]
+    public partial class MainPage : MasterDetailPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Inicial)));
         }
 
-        private void Open_Inicial(object sender, EventArgs e)
+        private async void Open_Inicial(object sender, EventArgs e)
         {
-
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Inicial)));
+                IsPresented = false;
+            }
+            
+            catch(Exception e)
+            {
+                await DisplayAlert("Ops!", e.Message, "OK");
+            }
         }
 
-        private void Open_Primeiro(object sender, EventArgs e)
+        private async void Open_Primeiro(object sender, EventArgs e)
         {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(ComponentesPrimeiro)));
+                IsPresented = false;
+            }
 
+            catch (Exception e)
+            {
+                await DisplayAlert("Ops!", e.Message, "OK");
+            }
         }
 
-        private void Open_Segundo(object sender, EventArgs e)
+        private async void Open_Segundo(object sender, EventArgs e)
         {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(ComponentesSegundo)));
+                IsPresented = false;
+            }
 
+            catch (Exception e)
+            {
+                await DisplayAlert("Ops!", e.Message, "OK");
+            }
         }
 
-        private void Open_Terceiro(object sender, EventArgs e)
+        private async void Open_Terceiro(object sender, EventArgs e)
         {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(ComponentesTerceiro)));
+                IsPresented = false;
+            }
 
+            catch (Exception e)
+            {
+                await DisplayAlert("Ops!", e.Message, "OK");
+            }
         }
 
-        private void Open_Vestibulinho(object sender, EventArgs e)
+        private async void Open_Vestibulinho(object sender, EventArgs e)
         {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Vestibulinho)));
+                IsPresented = false;
+            }
 
+            catch (Exception e)
+            {
+                await DisplayAlert("Ops!", e.Message, "OK");
+            }
         }
 
-        private void Open_Contato(object sender, EventArgs e)
+        private async void Open_Contato(object sender, EventArgs e)
         {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Contato)));
+                IsPresented = false;
+            }
 
+            catch (Exception e)
+            {
+                await DisplayAlert("Ops!", e.Message, "OK");
+            }
         }
     }
 }
